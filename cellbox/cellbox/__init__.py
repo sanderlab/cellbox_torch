d473ff17cf2d4cc97decfb4b61da177de4353ec4
Phuc Nguyen <phuc.nguyen@cchmc.org>
2023-07-06 17:56:09 -0400

    minor changes

--- cellbox/cellbox/__init__.py
+++ cellbox/cellbox/__init__.py
@@ -3,13 +3,15 @@ Import all necessary modules
 """
 from cellbox.config import Config
 from cellbox.model import *
+from cellbox.model_torch import *
 from cellbox.kernel import *
+from cellbox.kernel_torch import *
 from cellbox.dataset import *
 from cellbox.dataset_torch import *
-from cellbox.model_torch import *
 from cellbox.train import *
+from cellbox.train_torch import *
 from cellbox.utils import *
-from cellbox.kernel_torch import *
+from cellbox.utils_torch import *
 from cellbox.version import __version__, VERSION, get_msg
 
 get_msg()
